4c4d79267846b55c94726d5f4d7d3f838f7931bd
Petra <petra.defretes88@gmail.com>
2019-11-26 21:05:32 +0700

    add albums

--- routes.js
+++ routes.js
@@ -1,11 +1,19 @@
+//require express
 const express = require('express')
-
+//require techStacks.js di dalam folder controller
 const techStacksController = require('./controller/techStacks')
+const albumsController = require('./controller/albums')
 
+//require Router
 const Router = express.Router()
 
 Router.get('/tech-stacks', techStacksController.getAll)
 Router.post('/tech-stacks', techStacksController.getinsert)
 Router.delete('/tech-stacks/:id', techStacksController.delete)
 
+Router.get('/albums', albumsController.getAll)
+Router.post('/albums', 
+    albumsController.handleUpload,
+    albumsController.insert)
+Router.delete('/albums/:id', albumsController.delete)
 module.exports = Router
